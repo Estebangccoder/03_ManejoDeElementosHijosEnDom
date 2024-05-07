7826b6e6e8fe9957cfd523420bb4aff1a1c8f440
estebangccoder <estebangccoder@gmail.com>
2024-05-07 13:37:39 -0500

    Clase de manejo de hermanos

--- src/js/main.js
+++ src/js/main.js
@@ -1,65 +1,58 @@
-const main=document.querySelector("main")
-const boton=document.querySelector(".boton_agregar")
-const botonEliminar=document.querySelector(".boton_eliminar")
-const botonReemplazar=document.querySelector(".boton_reemplazar")
-const botonEnMedioDe=document.querySelector(".boton_middle_of")
+const header =document.querySelector("header")
+const main =document.querySelector("main")
+const footer =document.querySelector("footer")
+const btnExample= createButtonInfo()
 
-//Creacion de un nuevo boton
-function createButtonSuccess() {
-    const button=document.createElement("button")
-   // button.setAttribute("type","button") //enviar el atributo type al elemento button
-    button.type = "button" // otra forma de enviar el atributo type al elemento button version moderna
-    button.classList.add("btn", "btn-success") //agregamos las clases al elemento button
-    button.textContent = "hello" //agregamos contenido de texto al elemento button
-    return button
+
+
+//Insert element before the container
+// main.before(btnExample)
+
+//Insert element like the last child
+// main.appendChild(btnExample)
+
+// Insert element after the container
+//main.after(btnExample)
+
+//Insert element like a first child
+// main.prepend(btnExample)
+
+
+// Insert a lot of elements like a last childrens
+// main.append(btnExample)
+
+
+// // Replace a first child 
+// main.replaceChild(btnExample)
+
+// Replace all children
+// main.replaceChildren(btnExample)
+
+// Borra el contenedero
+// main.replaceWith(btnExample)
+
+//borra por completo el elemento
+// main.remove()
+
+
+function createButtonInfo(){
+    const btn = document.createElement("button")
+    btn.classList.add("btn", "btn-info")
+    btn.textContent = "Point"
+    return btn
 }
 
+// console.log(main)
+// console.log(main.children)
+// // acceder al padre del elemento
+// const btnDark =document.querySelector(".btn-dark")
+// console.log(btnDark.parentElement)
 
-// recepcion del evento click y agregar el boton
-boton.addEventListener("click",function() {
-//    alert("Hola")
-    const btnResponse=createButtonSuccess()
-    // console.log(btnResponse)
-    main.appendChild(btnResponse) //esta es una forma de insertar con la funcion
-    // main.innerHTML+=`  <button type="button" class="btn btn-primary btn-success">
-    //         Hello
-    //         </button>
-    // `// esta es otra forma de agregar pero con etiquetas HTML
-})
+// //Acceder al primer hijo del elemento
+// console.log(main.firstElementChild)
+// //Acceder al ultimo hijo del elemento
+// console.log(main.lastChild)
 
-
-//Eliminar
-botonEliminar.addEventListener("click",function() {
-   
-    const btnForDeleted=selectInfoButton()
-    main.removeChild(btnForDeleted)
-       
-        
-})
-
-
-//Seleccion del boton
-function selectInfoButton(){
-    const btnFound=document.querySelector(".btn-info")
-    return btnFound
-}
-
-//Reemplazar
-
-botonReemplazar.addEventListener("click",function(){
-    const btnForReplaced=selectInfoButton()
-    const newButton=createButtonSuccess()
-    main.replaceChild(newButton,btnForReplaced)
-       
-
-})
-
-
-//Insert before
-
-botonEnMedioDe.addEventListener("click",function(){
-    const buttonInMiddle= createButtonSuccess()
-    const btnReference=selectInfoButton()
-    main.insertBefore(buttonInMiddle,btnReference)
-})
-
+console.log(main.previousElementSibling) // Acceder al elemento hermano anterior
+console.log(main) //acceder al elemento
+console.log(main.nextElementSibling) //Acceder al elemento hermano siguiente
